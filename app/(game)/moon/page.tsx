350b5d56bdafe313512120357962c45bfd9d653e
ultrocharge <oliver.elias011@gmail.com>
2024-09-27 10:09:49 -0700

    refactor: add the modal for points and products on Moon Page

--- app/(game)/moon/page.tsx
+++ app/(game)/moon/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Image from "next/image";
 import { FaMoon } from "react-icons/fa";
 import Link from "next/link";
 import Script from "next/script";
+
 const footer = [
     {
         Icon:<Image
@@ -48,7 +49,8 @@ const footer = [
 export default function Moon() {
     const count = 0;
     const username = "MoverzBot"
-
+    const [visible, setVisible] = useState(false)
+    const [currentTitle, setCurrentTitle] = useState('')
     useEffect(() => {  
         if (typeof window !== 'undefined') {  
             if (window.Telegram && window.Telegram.WebApp) {  
@@ -61,15 +63,92 @@ export default function Moon() {
         }  
     }, []);
 
+    const open = (title: string) => {
+        setVisible(true);
+        setCurrentTitle(title);
+    }
+
+    const points = (
+        <div className="flex flex-col justify-center gap-2 items-center">
+            <FaMoon size={60} color="yellow" className="mb-2" />
+            <div style={{ fontFamily: "'Brush Script MT', cursive", textShadow: "1px -20px 60px yellow"}} className="text-xl font-extrabold text-yellow-300 text-opacity-90 mb-3">
+                Points
+            </div>
+            <div style={{ fontFamily: "'Brush Script MT', cursive"}} className="font-bold text-white mb-1">
+                Represent your proportional share of 
+            </div>
+
+            <div className="flex flex-row justify-center items-center gap-2">
+                <div style={{ fontFamily: "'Brush Script MT', cursive"}} className="font-bold text-white mb-1">
+                    of 
+                </div>
+                <div>
+                    <Image
+                        src="/images/avatar.png"
+                        width={20}
+                        height={20}
+                        alt="this is bot app avatar"
+                    />
+                </div>
+                <div style={{ fontFamily: "'Brush Script MT', cursive"}} className="font-extrabold text-yellow-300 text-opacity-90 mb-1">
+                    $Moon AirDrop
+                </div>
+            </div>
+        </div>
+    )
+
+    const products = (
+        <div className="flex flex-col">
+            <div style={{ fontFamily: "'Brush Script MT', cursive"}} className="text-center text-xl font-extrabold text-yellow-300 text-opacity-90 mb-5">
+                Our Products
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="w-full rounded-xl py-2 text-sm flex gap-3 font-extrabold text-white justify-center items-center bg-green-300 bg-opacity-20" style={{ fontFamily: "'Brush Script MT', cursive", transform: "rotate(1deg)"}}>
+                    <div>
+                        <Image
+                            src="/images/moon.png"
+                            width={32}
+                            height={32}
+                            alt="this is bot app avatar"
+                        />
+                    </div>
+                    <div>Meme on Eclispe</div>
+                </div>
+                <div className="w-full rounded-xl py-2 text-sm flex gap-3 font-extrabold text-white justify-center items-center bg-green-300 bg-opacity-20" style={{ fontFamily: "'Brush Script MT', cursive", transform: "rotate(-1deg)"}}>
+                    <div>
+                        <Image
+                            src="/images/avatar.png"
+                            width={32}
+                            height={32}
+                            alt="this is bot app avatar"
+                        />
+                    </div>
+                    <div>Mini Apps</div>
+                </div>
+                <div className="w-full rounded-xl py-2 text-sm flex gap-3 font-extrabold text-white justify-center items-center bg-green-300 bg-opacity-20" style={{ fontFamily: "'Brush Script MT', cursive", transform: "rotate(1deg)"}}>
+                    <div>
+                        <Image
+                            src="/images/avatar.png"
+                            width={32}
+                            height={32}
+                            alt="this is bot app avatar"
+                        />
+                    </div>
+                    <div>Meme on Eclispe</div>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
-        <div className="max-w-sm mx-auto flex flex-col gap-5">
+        <div className="max-w-sm mx-auto flex flex-col gap-5 relative">
              <Script  
-        src="https://telegram.org/js/telegram-web-app.js"  
-        strategy="afterInteractive"  
-        onLoad={() => {  
-          console.log("Telegram Web App SDK loaded");  
-        }}  
-      /> 
+                src="https://telegram.org/js/telegram-web-app.js"  
+                strategy="afterInteractive"  
+                onLoad={() => {  
+                console.log("Telegram Web App SDK loaded");  
+                }}  
+            /> 
             <div className="w-full flex flex-row gap-3 border-b border-stone-700 py-3 justify-between px-4">
                 <div className="flex flex-1 ring-1 gap-3 justify-center ring-yellow-500 ring-opacity-40 rounded-2xl py-2 bg-yellow-500 bg-opacity-20">
                     <div className="flex items-center">
@@ -96,7 +175,7 @@ export default function Moon() {
 
             <div className="w-full flex flex-col gap-5 px-4">
                 <div className="flex flex-row justify-between items-center">
-                    <div className="flex gap-3 items-center ring-4 ring-stone-500 ring-opacity-70 py-1 px-3 bg-gray-600 bg-opacity-50 rounded-full w-60 overflow-hidden">
+                    <div className="flex gap-3 items-center ring-[3px] ring-stone-500 ring-opacity-70 py-1 px-3 bg-gray-600 bg-opacity-50 rounded-full w-60 overflow-hidden">
                         <div className="flex flex-row gap-2 items-center justify-center">
                             <div>
                                 <Image
@@ -106,7 +185,7 @@ export default function Moon() {
                                     alt="this is bot app avatar"
                                 />
                             </div>
-                            <div style={{ fontFamily: "'Brush Script MT', cursive"}} className="text-xl font-extrabold text-yellow-300 text-opacity-90">
+                            <div style={{ fontFamily: "'Brush Script MT', cursive", textShadow: "1px 1px 60px yellow"}} className="text-xl font-extrabold text-yellow-300 text-opacity-90 mb-1">
                                 mooner
                             </div>
                         </div>
@@ -115,17 +194,17 @@ export default function Moon() {
                             {username}
                         </div>
                     </div>
-                    <div className="w-12 h-12 flex items-center justify-center rounded-full bg-black image-with-shadow">
-                        <FaMoon size={25} style={{opacity: '70%'}} color="yellow"/>
+                    <div className="w-8 h-8 cursor-pointer flex items-center justify-center rounded-full bg-black image-with-shadow-icon" onClick={() => open('points')}>
+                        <FaMoon size={20} style={{opacity: '70%'}} color="yellow"/>
                     </div>
                 </div>
 
                 <div className="flex flex-row justify-between">
                     {footer.map((item, index) => (
                         <Link key={index} href={item.path}>
-                            <div className="ring-1 ring-yellow-400 border-t-2 border-t-yellow-400 w-28 h-28 text-xs font-extrabold gap-2 flex-col p-1 rounded-lg flex justify-center items-center bg-gray-600 bg-opacity-30" style={{ fontFamily: "'Brush Script MT', cursive"}}>
+                            <div className="ring-1 ring-yellow-400 border-t-2 border-t-yellow-400 w-28 h-28 text-xs font-extrabold flex-col p-1 rounded-lg flex justify-center items-center bg-gray-600 bg-opacity-30" style={{ fontFamily: "'Brush Script MT', cursive"}}>
                                 {item.Icon}
-                                <div className={`text-white text-lg font-extrabold shadow-lg ${item.Title === 'Moon Spin' ? 'mt-2' : ''}`} style={{ textShadow: "1px 1px 60px yellow"}}>
+                                <div className={`text-white text-base font-extrabold shadow-lg ${item.Title === 'Moon Spin' ? 'mt-2' : ''}`} style={{ textShadow: "1px 1px 60px yellow"}}>
                                     {item.Title}
                                 </div>
                             </div>
@@ -133,7 +212,21 @@ export default function Moon() {
                     ))}
                 </div>
 
-                <div className="box py-1 px-8 ring-2 ring-yellow-500 ring-opacity-90 font-extrabold flex items-center" style={{ fontFamily: "'Brush Script MT', cursive"}}>Our products</div>
+                <div className="box py-1 px-8 ring-2 ring-yellow-500 ring-opacity-90 font-extrabold flex items-center" onClick={() => open('products')} style={{ fontFamily: "'Brush Script MT', cursive"}}>Our products</div>
+
+                <div className={`${visible ? 'flex' : 'hidden'} absolute top-0 left-0 w-full h-screen bg-gray-700 bg-opacity-45 justify-center items-center px-5`}>
+                    <div className="w-full flex flex-col rounded-lg bg-[#1B1B1B] p-5 border border-solid border-gray-400 border-opacity-20">
+                        <div className="flex justify-end">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="10" height="10" overflow="visible" stroke="white" strokeWidth="10" strokeLinecap="round" onClick={() => setVisible(false)}>
+                                <line x1="0" y1="0" x2="50" y2="50" />
+                                <line x1="50" y1="0" x2="0" y2="50" />
+                            </svg>
+                        </div>
+
+                        { currentTitle === 'points' ? points : null}
+                        { currentTitle === 'products' ? products : null}
+                    </div>
+                </div>
             </div>
         </div>
     )
